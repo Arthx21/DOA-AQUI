48898e020c60e70395d2aa92abf250aba00785a7
Arthx21 <arthur.game21carvalho@gmail.com>
2025-10-14 13:21:12 -0300

    apagou o restante

--- src/back/Programa/obj/Debug/net9.0/Programa.AssemblyInfo.cs
+++ src/back/Programa/obj/Debug/net9.0/Programa.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Programa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcf81cd63d2d9ecb8f06ad7e81d92792c41ac3ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fe258e4205aff5aa25a15ac5e99533aa2f23b3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Programa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Programa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
